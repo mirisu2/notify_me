e51547d1c8b46df8a2e063765f3223aae70deb27
arty <arty3818@gmail.com>
2020-08-19 14:09:28 +0300

    Initial commit

---
+++ queue_service/app/__init__.py
@@ -0,0 +1,53 @@
+import logging
+from os import environ
+
+from flask import Flask, request
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask_redis import FlaskRedis
+
+app = Flask(__name__)
+app.config['DEBUG'] = environ.get('DEBUG')
+app.config['ENV'] = environ.get('ENV')
+app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+app.config['REDIS_URL'] = environ.get('REDIS_URL')
+app.config['ALLOWED_TOKENS'] = environ.get('ALLOWED_TOKENS')
+app.config['CHANNEL'] = environ.get('CHANNEL')
+redis_client = FlaskRedis(app)
+
+logger = logging.getLogger('__notify__')
+ch_console = logging.StreamHandler()
+ch_console.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+ch_console.setFormatter(formatter)
+logger.addHandler(ch_console)
+
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=[],
+    headers_enabled=True,
+    strategy='fixed-window-elastic-expiry'
+)
+limiter.logger.addHandler(ch_console)
+
+
+@limiter.request_filter
+def ip_whitelist():
+    return request.remote_addr == "127.0.0.1"
+
+
+from app.api import email, telegram
+
+
+limiter.limit("10 per second")(email.bp)
+limiter.limit("10 per second")(telegram.bp)
+
+app.register_blueprint(email.bp)
+app.register_blueprint(telegram.bp)
+
+
+@app.route("/ping")
+@limiter.limit("5 per minute", override_defaults=False)
+def ping():
+    return "PONG"
---
+++ queue_service/app/api/email/__init__.py
@@ -0,0 +1,31 @@
+import redis
+from flask import Blueprint, request, jsonify
+
+from app import app, redis_client
+
+bp = Blueprint('email', __name__, url_prefix='/api')
+
+
+@bp.before_request
+def check_api_header():
+    header_api_key = request.headers.get('X-Notify-Api-Key')
+    if not header_api_key:
+        return jsonify({'error': 'The request header does not have an API access key'}), 403
+    if header_api_key not in app.config['ALLOWED_TOKENS']:
+        return jsonify({'error': 'Access denied'}), 403
+
+
+@bp.route('/email', methods=['POST'])
+def email():
+    i = request.get_json()
+    try:
+        data = i['type'] + '1#2#3' + i['address'] + '1#2#3' + i['message']
+    except KeyError as e:
+        print(e)
+        return 'KeyError', 404
+    try:
+        redis_client.publish(app.config['CHANNEL'], data)
+        return "OK", 200
+    except redis.exceptions.ConnectionError as e:
+        print(e)
+        return 'Something went wrong', 404
